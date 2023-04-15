f40610d0c084b3d1a210f3a5684ce67524ee7281
JuanCarlosHenaoLopez <102966888+JuanCarlosHenaoLopez@users.noreply.github.com>
2023-04-15 08:43:06 -0500

    Commit inicial configuracion ambiente

---
+++ src/main/java/com/tesis/inmobiliaria360/Inmobiliaria360Application.java
@@ -0,0 +1,13 @@
+package com.tesis.inmobiliaria360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inmobiliaria360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Inmobiliaria360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tesis/inmobiliaria360/Inmobiliaria360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesis.inmobiliaria360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Inmobiliaria360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
